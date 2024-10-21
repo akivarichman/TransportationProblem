d8d3ea11a05e39a0a18d8de00b4974c83c14ec25
akivarichman <121504299+akivarichman@users.noreply.github.com>
2024-10-20 20:03:21 -0400

    solution to VAM

--- Python Code/transportationProblem.py
+++ Python Code/transportationProblem.py
@@ -26,77 +26,78 @@ columns = len(demand)
 
 
 
-
-
-
-
-# dictionary where key => the tuple (row, col) & value => the cost
-costdict = {}
-for row in range(rows):
-    for col in range(columns):
-        costdict[row, col] = costs[row][col]
+penalties = {}
+creatingPenalties = True
+row_num = 0
+col_num = 0
 
 # penalty dictionary
-penaltydict = {}
-for row in range(rows):
-    min1 = 100 # really max int
-    min2 = 100 # really max int
-    for col in range(columns):
-        if(costs[row][col] < min1):
-            min2 = min1
-            min1 = costs[row][col]
-        elif(costs[row][col] < min2):
-            min2 = costs[row][col]
-    penaltydict["r",row] = min2 - min1
-for col in range(columns):
-    min1 = 100 # really max int
-    min2 = 100 # really max int
+def updatePenalties(penaltydict):
     for row in range(rows):
-        if(costs[row][col] < min1):
-            min2 = min1
-            min1 = costs[row][col]
-        elif(costs[row][col] < min2):
-            min2 = costs[row][col]
-    penaltydict["c",col] = min2 - min1
-
-print(penaltydict)
-
-# find max from penatly dict
-largest_key = max(penaltydict, key=penaltydict.get)
-dir, num = largest_key
-
-col_num = 1000
-row_num = num
-
-if(dir == 'r'):
-    mini = 100 # really max int
+        if(creatingPenalties or penaltydict['r', row] >= 0):
+            min1 = 100 # really max int
+            min2 = 100 # really max int
+            for col in range(columns):
+                if(creatingPenalties or penaltydict['c', col] >= 0):
+                    if(costs[row][col] < min1):
+                        min2 = min1
+                        min1 = costs[row][col]
+                    elif(costs[row][col] < min2):
+                        min2 = costs[row][col]
+            penaltydict["r",row] = min2 - min1
     for col in range(columns):
-        if(costdict[row_num, col] < mini):
-            print(mini)
-            message = 'we place allocation at row ', num, 'column ', col
-            col_num = col
-            mini = costdict[row_num, col]
+        if(creatingPenalties or penaltydict['c', col] >= 0):
+            min1 = 100 # really max int
+            min2 = 100 # really max int
+            for row in range(rows):
+                if(creatingPenalties or penaltydict['r', row] >= 0):
+                    if(costs[row][col] < min1):
+                        min2 = min1
+                        min1 = costs[row][col]
+                    elif(costs[row][col] < min2):
+                        min2 = costs[row][col]
+            penaltydict["c",col] = min2 - min1
 
-print()
+# find where to place allocation based on min cost
+def identifyCellToAllocate(direction, costs, row_num, col_num, penalties):
+    if(direction == 'r'):
+        mini = 100 # really max int
+        for col in range(columns):
+            if(penalties['c', col] >= 0 and costs[row_num][col] < mini):
+                col_num = col
+                mini = costs[row_num][col]
+    if(direction == 'c'):
+        mini = 100 # really max int
+        for row in range(rows):
+            if(penalties['r', row] >= 0 and costs[row][col_num] < mini):
+                row_num = row
+                mini = costs[row][col_num]
+    return [row_num, col_num]
 
-print(row_num)
-print(col_num)
 
-allocation = min(supply[row_num], demand[col_num])
-solution[row_num][col_num] = allocation
-supply[row_num] = supply[row_num] - allocation
-demand[col_num] = demand[col_num] - allocation
+while total > 0:
+    #determining the penalties of each row and column
+    updatePenalties(penalties)
+    creatingPenalties = False
+    # finding the maximum penatly
+    largest_penalty_key = max(penalties, key=penalties.get)
+    direction, num = largest_penalty_key
+    if(direction == 'r'):
+        row_num = num
+        penalties['r', num] = -1
+    else:
+        col_num = num
+        penalties['c', num] = -1
+    # allocating to min cost cell in that row / col with max penalty
+    row_num, col_num = identifyCellToAllocate(direction, costs, row_num, col_num, penalties)
+    allocation = min(supply[row_num], demand[col_num])
+    solution[row_num][col_num] = allocation
+    supply[row_num] = supply[row_num] - allocation
+    demand[col_num] = demand[col_num] - allocation
+    total = total - allocation
 
-print()
 print(solution)
-print(supply)
-print(demand)
-
-# while total > 0:
-    
 
 
 
 # even checks ones that were cancelled out already
-
-print()
